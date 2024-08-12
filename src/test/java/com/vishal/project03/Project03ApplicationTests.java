15bbc569f2ca51f3e3eb0d7f5e27163f3bf2c40c
vishal.yadav <vishal.yadav@qsstechnosoft.com>
2024-08-12 12:03:51 +0530

    1st commit

---
+++ src/test/java/com/vishal/project03/Project03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishal.project03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
